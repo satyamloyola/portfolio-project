f927a6ab108cab7ceaf0a042ec340ad35826536f
Satyam sinha <satyamloyola@gmail.com>
2020-06-01 23:32:06 +0530

    Added blog functionality and polish

--- blog/models.py
+++ blog/models.py
@@ -6,4 +6,14 @@ class Blog(models.Model):
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
 
+    def __str__(self):          #This function is used to display particular object from django admin.
+        return self.title
+
+    def summary(self):
+        return self.body[:100]
+
+    def pub_date_preety(self):
+        return self.pub_date.strftime('%b %e %Y')
+
+
     
